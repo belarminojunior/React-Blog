4d4bbb65966a32763a18063ef775bdfc71b8e51b
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2024-01-31 23:37:40 +0200

    RRv6

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Nav = ({ search, setSearch }) => {
+  return (
+    <nav className="Nav">
+      <form
+        action=""
+        className="searchForm"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <label htmlFor="search">Search Posts</label>
+        <input
+          type="text"
+          id="search"
+          placeholder="Search Posts"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="post">Post</Link>
+          <Link to="about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
