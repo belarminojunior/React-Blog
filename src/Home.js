c9bda3d060781e2b2e3cefb6490c09146fc656a7
Belarmino Simão Júnior <belarminosimaojunior@gmail.com>
2024-02-06 14:54:38 +0200

    hooks

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,23 @@ import React from "react";
 
 import Feed from "./Feed";
 
-const Home = ({ posts }) => {
+const Home = ({ posts, fetchError, isLoading }) => {
   return (
     <main className="Home">
-      {posts.length ? (
-        <Feed posts={posts} />
-      ) : (
-        <p style={{ marginTop: "2rem" }}>No Posts To Display</p>
+      {isLoading && <p className="statusMessage">Loading Posts...</p>}
+      {!isLoading && fetchError && (
+        <p className="statusMessage" style={{ color: "red" }}>
+          {fetchError}
+        </p>
       )}
+
+      {!isLoading &&
+        !fetchError &&
+        (posts.length ? (
+          <Feed posts={posts} />
+        ) : (
+          <p className="statusMessage">No Posts to display!</p>
+        ))}
     </main>
   );
 };
--- src/Layout.js
+++ src/Layout.js
@@ -6,10 +6,10 @@ import Footer from "./Footer";
 
 import { Outlet } from "react-router-dom";
 
-const Layout = ({ search, setSearch }) => {
+const Layout = ({ search, setSearch, width }) => {
   return (
     <div>
-      <Header title="React JS Blog" />
+      <Header title="React JS Blog" width={width} />
       <Nav search={search} setSearch={setSearch} />
       <Outlet />
       <Footer />
